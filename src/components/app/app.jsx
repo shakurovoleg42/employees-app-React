716d167a0bed52836edb9f2dea826b424679b0f5
Oleg Shakurov <thorsonodin2003@gmail.com>
2023-05-25 19:31:22 +0500

    modified:   src/components/app/app.jsx 	modified:   src/components/employers-list-item/employers-list-item.jsx 	modified:   src/components/employers-list/employers-list.jsx

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -7,6 +7,15 @@ import EmployersAddForm from '../employers-add-form/employers-add-form';
 import './app.css';
 
 function App() {
+
+    const data = [
+        {name: 'Walter W.', salary: 500, increase: false, id: 1},
+        {name: 'Soul G.', salary: 800, increase: false, id: 2},
+        {name: 'Skyler W.', salary: 1500, increase: false, id: 3},
+        {name: 'Gustavo F.', salary: 2000, increase: false, id: 4},
+        {name: 'Oleg S.', salary: 3500, increase: false, id: 5},
+    ];
+
     return(
         <div className="app">
             <AppInfo/>
@@ -16,7 +25,7 @@ function App() {
                 <AppFilter/>
             </div>  
 
-            <EmployersList/>  
+            <EmployersList data={data}/>  
             <EmployersAddForm/>     
         </div>
     );
