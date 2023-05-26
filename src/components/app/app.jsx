5c24306961a278c1c5f92241301b8054d4ba507e
Oleg Shakurov <thorsonodin2003@gmail.com>
2023-05-26 20:26:35 +0500

    modified:   src/components/app/app.jsx 	modified:   src/components/employers-add-form/employers-add-form.jsx 	modified:   src/components/employers-list-item/employers-list-item.jsx 	modified:   src/components/employers-list/employers-list.jsx

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,29 +8,45 @@ import EmployersAddForm from '../employers-add-form/employers-add-form';
 
 import './app.css';
 
-function App() {
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {name: 'Walter W.', salary: 500, increase: false, id: 1},
+                {name: 'Soul G.', salary: 800, increase: false, id: 2},
+                {name: 'Skyler W.', salary: 1500, increase: false, id: 3},
+                {name: 'Gustavo F.', salary: 2000, increase: false, id: 4},
+                {name: 'Oleg S.', salary: 3500, increase: false, id: 5}
+            ]  
+        }
+    }
 
-    const data = [
-        {name: 'Walter W.', salary: 500, increase: false, id: 1},
-        {name: 'Soul G.', salary: 800, increase: false, id: 2},
-        {name: 'Skyler W.', salary: 1500, increase: false, id: 3},
-        {name: 'Gustavo F.', salary: 2000, increase: false, id: 4},
-        {name: 'Oleg S.', salary: 3500, increase: false, id: 5},
-    ];
+    deletItem = (id) => {
+        this.setState(({data}) => {
+            return {
+                data: data.filter(item => item.id !== id)
+            }
+        })
+    }
 
-    return(
-        <div className="app">
-            <AppInfo/>
-
-            <div className="search-panel">
-                <SearchPanel/>
-                <AppFilter/>
-            </div>  
-
-            <EmployersList data={data}/>  
-            <EmployersAddForm/>     
-        </div>
-    );
+    render() {
+        return(
+            <div className="app">
+                <AppInfo/>
+    
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>  
+    
+                <EmployersList
+                    data={this.state.data}
+                    onDelete={this.deletItem}/>  
+                <EmployersAddForm/>     
+            </div>
+        );
+    }
 }
 
 export default App;
